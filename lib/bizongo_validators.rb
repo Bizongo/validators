8302daeb1c1b45c55cd2e1cdaca96f250c42f7e6
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2018-01-09 11:56:40 +0530

    Added validator code in main module

--- lib/bizongo_validators.rb
+++ lib/bizongo_validators.rb
@@ -1,12 +1,39 @@
 require "bizongo_validators/version"
-require "bizongo_validators/random_module"
+# require "bizongo_validators/random_module"
+require "active_record"
 
 module BizongoValidators
-  # module ContactNumberValidator
-  #   autoload :ContactNumberValidator, 'bizongo_validators/contact_number_validator'
-  # end
+  module ContactNumberValidator
+    extend ActiveSupport::Concern
 
-  def hello
-    autoload :RandomModule, 'bizongo_validators/random_module'
+    included do
+      class_attribute :contact_field
+
+      before_validation :format_contact_number, if: :contact_number_present
+      validate :validate_contact_number, if: :contact_number_present
+
+      private
+
+      def format_contact_number
+        self[contact_field] = "+91#{self[contact_field]}" if !self[contact_field].starts_with?("+91")
+      end
+
+      def validate_contact_number
+        errors.add(self.contact_field, " is not valid") if !self[contact_field].match(/^\+91[789][0-9]{9}/)
+      end
+
+      def contact_number_present
+        self[contact_field].present?
+      end
+
+    end
+
+    module ClassMethods
+      def contact_number_column_name(name)
+        binding.pry
+        self.contact_field = name
+      end
+    end
   end
+
 end
