9fa09dfc5a34b9a86e4cf3a8f2734112a9df01ce
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2018-01-09 13:19:10 +0530

    Modified to autoload validator

--- lib/bizongo_validators.rb
+++ lib/bizongo_validators.rb
@@ -1,39 +1,8 @@
 require "bizongo_validators/version"
 # require "bizongo_validators/random_module"
 require "active_record"
+# require "bizongo_validators/contact_number_validator"
 
 module BizongoValidators
-  module ContactNumberValidator
-    extend ActiveSupport::Concern
-
-    included do
-      class_attribute :contact_field
-
-      before_validation :format_contact_number, if: :contact_number_present
-      validate :validate_contact_number, if: :contact_number_present
-
-      private
-
-      def format_contact_number
-        self[contact_field] = "+91#{self[contact_field]}" if !self[contact_field].starts_with?("+91")
-      end
-
-      def validate_contact_number
-        errors.add(self.contact_field, " is not valid") if !self[contact_field].match(/^\+91[789][0-9]{9}/)
-      end
-
-      def contact_number_present
-        self[contact_field].present?
-      end
-
-    end
-
-    module ClassMethods
-      def contact_number_column_name(name)
-        binding.pry
-        self.contact_field = name
-      end
-    end
-  end
-
+  autoload :ContactNumberValidator, 'bizongo_validators/contact_number_validator'
 end
--- lib/bizongo_validators/contact_number_validator.rb
+++ lib/bizongo_validators/contact_number_validator.rb
@@ -1,34 +1,36 @@
 require 'active_record'
 
-module ContactNumberValidator
-  extend ActiveSupport::Concern
+module BizongoValidators
+  module ContactNumberValidator
+    extend ActiveSupport::Concern
 
-  included do
-    class_attribute :contact_field
+    included do
+      class_attribute :contact_field
 
-    before_validation :format_contact_number, if: :contact_number_present
-    validate :validate_contact_number, if: :contact_number_present
+      before_validation :format_contact_number, if: :contact_number_present
+      validate :validate_contact_number, if: :contact_number_present
 
-    private
+      private
 
-    def format_contact_number
-      self[contact_field] = "+91#{self[contact_field]}" if !self[contact_field].starts_with?("+91")
+      def format_contact_number
+        self[contact_field] = "+91#{self[contact_field]}" if !self[contact_field].starts_with?("+91")
+      end
+
+      def validate_contact_number
+        errors.add(self.contact_field, " is not valid") if !self[contact_field].match(/^\+91[789][0-9]{9}/)
+      end
+
+      def contact_number_present
+        self[contact_field].present?
+      end
+
     end
 
-    def validate_contact_number
-      errors.add(self.contact_field, " is not valid") if !self[contact_field].match(/^\+91[789][0-9]{9}/)
-    end
-
-    def contact_number_present
-      self[contact_field].present?
-    end
-
-  end
-
-  module ClassMethods
-    def contact_number_column_name(name)
-      binding.pry
-      self.contact_field = name
+    module ClassMethods
+      def contact_number_column_name(name)
+        binding.pry
+        self.contact_field = name
+      end
     end
   end
 end
