72e22bf8516842c329d8e5780e3e182be6b43518
mayankkumar25 <mayank.kumar@bizongo.com>
2019-12-03 16:48:32 +0530

    added data

--- lib/validators/contact_number_validator.rb
+++ lib/validators/contact_number_validator.rb
@@ -5,56 +5,50 @@ module ContactNumberValidator
   extend ActiveSupport::Concern
 
   included do
-    class_attribute :contact_field, :country_details
+    class_attribute :contact_field, :country_short_name
 
-    before_validation :format_contact_number, if: :is_indian_flow
-    validate :validate_indian_contact_number, if: :is_indian_flow
-    validate :validate_international_contact_number, unless: :is_indian_flow
+    before_validation :format_contact_number, if: :indian_flow
+    validate :validate_indian_contact_number, if: :indian_flow
+    validate :validate_international_contact_number, unless: :indian_flow
 
     private
 
     def format_contact_number
-      country = self[country_details]
-      if country.present? and country['countrySortName'].present?
-        return
-      end
+      binding.pry
+      return if country_short_name.present?
       self[contact_field] = "+91#{self[contact_field]}" if !self[contact_field].starts_with?("+91")
     end
 
     def validate_indian_contact_number
-      country = self[country_details]
-      if country.present? and country['countrySortName'].present?
+      binding.pry
+      if country_short_name.present?
         errors.add(self.contact_field, " is not valid") if !self[contact_field].match(/^[6789][0-9]{9}/)
         return
       end
-      errors.add(self.contact_field, " is not valid") if !self[  contact_field].match(/^\+91[6789][0-9]{9}/)
+      errors.add(self.contact_field, " is not valid") if !self[contact_field].match(/^\+91[6789][0-9]{9}/)
     end
 
     def validate_international_contact_number
+      binding.pry
       return if self[contact_field].blank?
-      country = self[country_details]["countrySortName"]
-      errors.add(self.contact_field, " is not valid") unless Phonelib.valid_for_country? self[contact_field], country
+
+      errors.add(self.contact_field, " is not valid") unless Phonelib.valid_for_country? self[contact_field], country_short_name
     end
 
-    # def contact_number_present
-    #   self[contact_field].present?
-    # end
-
-    def is_indian_flow
+    def indian_flow
       return false if self[contact_field].blank?
 
-      country = self[country_details]
-      return false if country.present? and
-          country['countrySortName'].present? and country['countrySortName'] != 'in'
+      return false if country_short_name.present? and country_short_name != 'in'
 
       true
     end
   end
 
   module ClassMethods
-    def contact_number_column_name(name, mobile_number_country_details = nil)
+    def contact_number_column_name(name, country_short_name=nil)
+      binding.pry
       self.contact_field = name
-      self.country_details = mobile_number_country_details
+      self.country_short_name = country_short_name
     end
   end
 end
