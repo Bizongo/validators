24b323ae8df7e199e9eba63cd162a42296b3728f
mayankkumar25 <mayank.kumar@bizongo.com>
2019-12-04 02:40:13 +0530

    added check by countryShortName

--- lib/validators/contact_number_validator.rb
+++ lib/validators/contact_number_validator.rb
@@ -5,7 +5,7 @@ module ContactNumberValidator
   extend ActiveSupport::Concern
 
   included do
-    class_attribute :contact_field, :country_short_name
+    class_attribute :contact_field, :country_short_name_fetching_function
 
     before_validation :format_contact_number, if: :indian_flow
     validate :validate_indian_contact_number, if: :indian_flow
@@ -13,15 +13,19 @@ module ContactNumberValidator
 
     private
 
+    def country_short_name
+      return self.send(country_short_name_fetching_function) if country_short_name_fetching_function.present?
+
+      nil
+    end
+
     def format_contact_number
-      binding.pry
-      return if country_short_name.present?
+      return if country_short_name
       self[contact_field] = "+91#{self[contact_field]}" if !self[contact_field].starts_with?("+91")
     end
 
     def validate_indian_contact_number
-      binding.pry
-      if country_short_name.present?
+      if country_short_name
         errors.add(self.contact_field, " is not valid") if !self[contact_field].match(/^[6789][0-9]{9}/)
         return
       end
@@ -29,26 +33,28 @@ module ContactNumberValidator
     end
 
     def validate_international_contact_number
-      binding.pry
       return if self[contact_field].blank?
 
-      errors.add(self.contact_field, " is not valid") unless Phonelib.valid_for_country? self[contact_field], country_short_name
+      errors.add(self.contact_field, " is not valid") unless Phonelib.valid_for_country? self[contact_field],
+                                                                                         country_short_name
     end
 
     def indian_flow
+      country_short_name = nil
+      country_short_name = self.send(country_short_name_fetching_function) if country_short_name_fetching_function.present?
+
       return false if self[contact_field].blank?
 
-      return false if country_short_name.present? and country_short_name != 'in'
+      return false if country_short_name.present? and (country_short_name != 'in' or country_short_name != 'IN')
 
       true
     end
   end
 
   module ClassMethods
-    def contact_number_column_name(name, country_short_name=nil)
-      binding.pry
+    def contact_number_column_name(name, country_short_name_fetching_function=nil)
       self.contact_field = name
-      self.country_short_name = country_short_name
+      self.country_short_name_fetching_function = country_short_name_fetching_function
     end
   end
 end
